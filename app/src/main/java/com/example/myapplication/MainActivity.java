c18d85bcfe094b3b80cddaecbdbd9df4a18eea22
Bastian92 <bastian.martinez92@gmail.com>
2022-11-24 23:11:30 -0400

    etapa 1
    se generaron las pantallas y se conectaron, y se puso un contador de caracteres hasta 20

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.myapplication;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //referencias
+    Button buscar;
+    EditText etTitulo,etDescripcion;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //validar campos vacios
+        etTitulo=(EditText) findViewById(R.id.etTitulo);
+        etDescripcion=(EditText) findViewById(R.id.etDescripcion);
+
+
+        //boton buscar pasa a la siguiente pagina
+        buscar = (Button) findViewById(R.id.btnBuscar);
+
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b = new Intent(MainActivity.this,SegundaPantallaBuscar.class);
+                startActivity(b);
+
+            }
+        });
+
     }
+
+
+
 }
